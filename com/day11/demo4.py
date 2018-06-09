81c07246730cf5309ecdccd67f0865d26ad72b3f
[Edwardyuhuashang] <[yuhuashang@aliyun.com]>
2018-06-09 19:18:14 +0800

    测试Python

---
+++ com/day11/demo4.py
@@ -0,0 +1,68 @@
+'''test = "李杰"
+for item in test:
+    print(item)
+'''
+
+# test = 5
+# bit_len = test.bit_length()
+# print(bit_len)
+#
+# a = "alex"
+# b = a.capitalize()
+# print(a)
+# print(b)
+
+# name = " aleX"
+# a = name.strip()
+# print(a, len(a))
+
+# name = " aleX"
+# a = name.startswith('al')
+# print(a)
+
+# name = " aleX"
+# a = name.endswith('X')
+# print(a)
+
+# name = " aleX"
+# a = name.replace('l', 'p')
+# print(a)
+
+# name = " aleX"
+# # 不包含被切割元素, 得到list[集合]
+# a = name.split('l')
+# # 包含切割元素, 得到tuple[元组]
+# b = name.partition('l')
+# print(a, type(a))
+# print(b, type(b))
+
+# name = " aleX"
+# a = name.upper()
+# print(a)
+
+# name = " aleX"
+# a = name.lower()
+# print(a)
+
+# name = " aleX"
+# for item in name:
+#
+#     print(item)
+
+# name = " aleX"
+# a = name.index('a')
+# print(a)
+
+# name = " aleX"
+# a = name[1]
+# print(a)
+
+# name = " aleX"
+# a = name[0:3]
+# print(a)
+
+name = " aleX"
+length = len(name)
+print(length)
+a = name[length-2:length]
+print(a)
